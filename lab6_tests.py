4266337e21bb2760b274ce77bec3858f4667a585
annikaweiland <amweilan@calpoly.edu>
2024-11-03 16:26:35 -0800

    Lab 6 submission

--- lab6_tests.py
+++ lab6_tests.py
@@ -64,18 +64,53 @@ class TestCases(unittest.TestCase):
 
 
     # Part 1
+    def test_selection_sort_books(self):
+        book1 = data.Book(["kendall", "kylie"], "Hunger Games")
+        book2 = data.Book(["David"], "Almost")
+        book3 = data.Book(["Hey", "Annika"], "Normal People")
+        expected = lab6.selection_sort_books([book1, book3, book2])
+        result = [data.Book(['David'], 'Almost'), data.Book(['kendall', 'kylie'], 'Hunger Games'), data.Book(['Hey', 'Annika'], 'Normal People')]
+        self.assertEqual(expected, result)
 
+    def test_selection_sort_books_2(self):
+        book1 = data.Book(["Annika"], "I love electrical engineering")
+        book2 = data.Book(["Daivd", "bob"], "Everything")
+        expected  = [data.Book(["Daivd", "bob"], "Everything"), data.Book(["Annika"], "I love electrical engineering")]
+        result = lab6.selection_sort_books([book1, book2])
+        self.assertEqual(expected, result)
 
     # Part 2
+    def test_swap_case(self):
+        result = lab6.swap_case("Hey!")
+        expected = "hEY!"
+        self.assertEqual(expected, result)
 
+    def test_swap_case_2(self):
+        result = lab6.swap_case("CUIDATE, por favor!!")
+        expected = "cuidate, POR FAVOR!!"
+        self.assertEqual(expected, result)
 
     # Part 3
+    def test_str_translate(self):
+        result = lab6.str_translate("Lalalala", 'a', 'e')
+        expected = ('Lelelele')
+        self.assertEqual(result, expected)
 
+    def test_str_translate_2(self):
+        result = 'ji jow jave you been jayden'
+        expected = lab6.str_translate('hi how have you been hayden', 'h', 'j')
+        self.assertEqual(result, expected)
 
     # Part 4
+    def test_histogram(self):
+        result = lab6.histogram("This is so so so cool")
+        expected = {'This': 1, 'is': 1, 'so': 3, 'cool':1}
+        self.assertEqual(result, expected)
 
-
-
+    def test_histogram_2(self):
+        result = lab6.histogram("oh my goodness, oh my gosh!")
+        expected = {'oh': 2, 'my': 2, 'goodness,':1, 'gosh!':1}
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
